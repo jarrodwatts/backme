bbc0c72c5f3a2469806fc1eba089902705175467
Jarrod Watts <jarrodwatts16@gmail.com>
2023-10-26 21:17:26 +1100

    Don't hardcode public gateway URL

--- lib/useUpload.ts
+++ lib/useUpload.ts
@@ -1,10 +1,9 @@
-import { useSDK } from "@thirdweb-dev/react";
+import { useSDK, useStorage } from "@thirdweb-dev/react";
 
 export default function useUpload() {
   const sdk = useSDK();
+  const storage = useStorage();
 
-  // Return a function that follows this signature:
-  // (data: unknown) => Promise<string>
   return async function upload(data: unknown) {
     let ipfsHash: string = "";
 
@@ -18,11 +17,26 @@ export default function useUpload() {
 
     const [cid, path] = ipfsHash.replace("ipfs://", "").split("/");
 
-    const publicUrl = `https://gateway.ipfscdn.io/ipfs/${cid}/${path}`;
+    // See what gateways you have available, as gateway URL is unique to your thirdweb API key.
+    const availableGateways = storage?.getGatewayUrls();
 
-    // Need to fetch so that the data is ready when we finish this function
-    await fetch(publicUrl);
+    // Use first available URL for IPFS
+    const gatewayUrlRoot = availableGateways?.["ipfs://"][0]
+      ?.split("/")
+      ?.slice(0, -2)
+      ?.join("/");
 
-    return publicUrl;
+    // Form the URL to the file with that gateway
+    const gatewayUrl = `${gatewayUrlRoot}/${cid}/${path}`;
+
+    // We need to fetch so that the data is ready when we finish this function...
+    // This is kind of stupid, but Lens does a check to see if the URL is valid.
+    // If we return the URL before it's ready to be served by the gateway, Lens
+    // will throw an error saying the file is not found cuz it's not ready yet.
+    // So, by fetching, we kind of wait for the file to be ready to be served before returning the URL
+    // to avoid the error.
+    await fetch(gatewayUrl);
+
+    return gatewayUrl;
   };
 }
